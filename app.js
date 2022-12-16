656b4cf8b537e6204bb73bde6589bcefb4b5d319
Midlaj13 <muhammedmidlaju@gmail.com>
2022-12-16 21:43:22 +0530

    adding home page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mongoose=require('mongoose')
 const app=express();
 
 
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
 app.listen(3000,()=>{
-    console.log("hello world!")
+    
 })
